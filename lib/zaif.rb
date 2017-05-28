e2ea41a61bfc72f9597c3a25c04757a6497c988d
char134217728 <d27d8c97e888tg@gmail.com>
2017-05-28 18:32:47 +0900

    timeout なくす

--- lib/zaif.rb
+++ lib/zaif.rb
@@ -180,8 +180,8 @@ module Zaif
             begin
                 https = Net::HTTP.new(uri.host, uri.port)
                 https.use_ssl = true
-                https.open_timeout = 10 
-                https.read_timeout = 30
+                #https.open_timeout = 10 
+                #https.read_timeout = 30
                 https.verify_mode = OpenSSL::SSL::VERIFY_PEER
                 https.verify_depth = 5
 
